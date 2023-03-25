720e00cdea0a00300e7c6b69fabf47f4e73506fc
Ivan <ivangalvan93@gmail.com>
2023-03-25 20:55:40 -0300

    Corrección de errores

--- src/main/java/com/portfolio/portfolioig/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/portfolioig/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://frontap-f0b8b.web.app/","http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontargprog-1.web.app","http://localhost:4200"})
 public class AuthController {
 
     @Autowired
